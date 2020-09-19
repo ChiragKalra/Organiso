85d65eb439d9e83cca05ae7b921c7a6082754a4e
bruhascended <arlakgarihc@gmail.com>
2020-09-19 21:04:31 +0530

    added copyright

--- app/src/main/java/com/bruhascended/sms/SettingsActivity.kt
+++ app/src/main/java/com/bruhascended/sms/SettingsActivity.kt
@@ -1,3 +1,19 @@
+/*
+                    Copyright 2020 Chirag Kalra
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 package com.bruhascended.sms
 
 import android.content.Intent
@@ -8,6 +24,7 @@ import androidx.preference.*
 import kotlinx.android.synthetic.main.activity_conversation.*
 
 
+@Suppress("unused")
 class SettingsActivity : AppCompatActivity(),
     PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
@@ -62,7 +79,12 @@ class SettingsActivity : AppCompatActivity(),
         }
         // Replace the existing Fragment with the new Fragment
         supportFragmentManager.beginTransaction()
-            .replace(R.id.settings, fragment)
+            .setCustomAnimations(
+                R.anim.slide_in_right,
+                android.R.anim.fade_out,
+                android.R.anim.fade_in,
+                android.R.anim.slide_out_right,
+            ).replace(R.id.settings, fragment)
             .addToBackStack(null)
             .commit()
         title = pref.title
@@ -71,22 +93,42 @@ class SettingsActivity : AppCompatActivity(),
 
     class HeaderFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.preferences, rootKey)
-
-            val notifPref: Preference = findPreference("notifications")!!
-            val githubPref: Preference = findPreference("github")!!
-            val websitePref: Preference = findPreference("website")!!
-            val bugPref: Preference = findPreference("report_bug")!!
-            val tabPref: SwitchPreferenceCompat = findPreference("promotions_category_visible")!!
+            setPreferencesFromResource(R.xml.header_preferences, rootKey)
+            val notifyPref: Preference = findPreference("notifications")!!
             val themePref: SwitchPreferenceCompat = findPreference("dark_theme")!!
 
-            notifPref.setOnPreferenceClickListener {
-                val intent = Intent()
-                intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
-                intent.putExtra("android.provider.extra.APP_PACKAGE", requireActivity().packageName)
+            notifyPref.setOnPreferenceClickListener {
+                val intent = Intent("android.settings.APP_NOTIFICATION_SETTINGS").apply {
+                    putExtra(
+                        "android.provider.extra.APP_PACKAGE",
+                        requireActivity().packageName
+                    )
+                }
                 requireActivity().startActivity(intent)
                 false
             }
+            themePref.setOnPreferenceChangeListener { _, _ ->
+                val sp = PreferenceManager.getDefaultSharedPreferences(requireActivity())
+                sp.edit().putBoolean("stateChanged", true).apply()
+                requireActivity().recreate()
+                true
+            }
+        }
+    }
+
+    class CategoryFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.category_preferences, rootKey)
+        }
+    }
+
+    class InfoFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.info_preferences, rootKey)
+
+            val githubPref: Preference = findPreference("github")!!
+            val websitePref: Preference = findPreference("website")!!
+            val bugPref: Preference = findPreference("report_bug")!!
 
             githubPref.setOnPreferenceClickListener {
                 val link = Uri.parse("https://github.com/ChiragKalra/Organiso")
@@ -103,18 +145,6 @@ class SettingsActivity : AppCompatActivity(),
                 false
             }
 
-            themePref.setOnPreferenceChangeListener { _, _ ->
-                val sp = PreferenceManager.getDefaultSharedPreferences(requireActivity())
-                sp.edit().putBoolean("stateChanged", true).apply()
-                requireActivity().recreate()
-                true
-            }
-
-            tabPref.setOnPreferenceChangeListener { _, _ ->
-                val sp = PreferenceManager.getDefaultSharedPreferences(requireActivity())
-                sp.edit().putBoolean("stateChanged", true).apply()
-                true
-            }
         }
     }
 }
